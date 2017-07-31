9a5c6c5551dbbcafa8081bd6b4856fb1d3490d5c
GaiYu0 <yg1246@nyu.edu>
2017-07-31 23:33:44 +0800

    cpu support + refactored loss

--- evaluate.py
+++ evaluate.py
@@ -1,56 +1,71 @@
+from pdb import set_trace as st
+
 from argparse import ArgumentParser
 import joblib
 import torch as th
 from torch.autograd import Variable
 from torch.optim import Adam
 from torch.utils.data import DataLoader, TensorDataset
-from loss import loss as loss_function
 from network import CNN, RNN
 from utilities import onehot, onehot_sequence, n_matches
 
 parser = ArgumentParser()
-parser.add_argument('--cnn-path', type=str, default='pretrained-cnn')
+parser.add_argument('--pretrained-cnn-path', type=str, default='pretrained-cnn')
+parser.add_argument('--cnn-path', type=str, default='')
 parser.add_argument('--batch-size', type=int, default=64)
 parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--interval', type=int, default=100)
+# --criterion=semi_cross_entropy/alternative_semi_cross_entropy/kl_loss/rl_loss
+parser.add_argument('--criterion', type=str, default='semi_cross_entropy')
+parser.add_argument('--model-path', type=str, default='')
 parser.add_argument('--n', type=int, default=3)
 parser.add_argument('--n-epochs', type=int, default=10)
-parser.add_argument('--n-units', type=int, default=0) # deactivate RNN if n-units is 0
+parser.add_argument('--n-units', type=int, default=0) # disable RNN if n-units is 0
 parser.add_argument('--pretrained-cnn', action='store_true', default=False)
 args = parser.parse_args()
 print args
 
-th.cuda.set_device(args.gpu)
+if args.gpu > -1:
+  cuda = True
+  th.cuda.set_device(args.gpu)
+else:
+  cuda = False
 
 training_data, training_labels = joblib.load('training-%d.data' % args.n)
 training_data = th.from_numpy(training_data)
-training_labels = onehot_sequence(th.from_numpy(training_labels), 10)
+training_labels = onehot_sequence(th.from_numpy(training_labels), 10, cuda)
 training_set = TensorDataset(training_data, training_labels)
 training_loader = DataLoader(training_set, args.batch_size)
 
 validation_data, validation_labels = joblib.load('validation-%d.data' % args.n)
 validation_data = th.from_numpy(validation_data)
-validation_labels = onehot_sequence(th.from_numpy(validation_labels), 10)
+validation_labels = onehot_sequence(th.from_numpy(validation_labels), 10, cuda)
 validation_set = TensorDataset(validation_data, validation_labels)
 validation_loader = DataLoader(validation_set, args.batch_size)
 
 test_data, test_labels = joblib.load('test-%d.data' % args.n)
 test_data = th.from_numpy(test_data)
-test_labels = onehot_sequence(th.from_numpy(test_labels), 10)
+test_labels = onehot_sequence(th.from_numpy(test_labels), 10, cuda)
 test_set = TensorDataset(test_data, test_labels)
 test_loader = DataLoader(test_set, args.batch_size)
 
 cnn_path = args.cnn_path if args.pretrained_cnn else None
 model = RNN(args.n_units, 10, cnn_path)
-model.cuda()
+if args.gpu > -1:
+  model.cuda()
+criterion = getattr(__import__('loss'), args.criterion)()
+if args.gpu > -1:
+  criterion.cuda()
 optimizer = Adam(model.parameters(), lr=1e-3)
 
 for epoch in range(args.n_epochs):
   for index, batch in enumerate(training_loader):
     data, labels = batch
-    data, labels = Variable(data.cuda()), Variable(labels.cuda())
+    if args.gpu > -1:
+      data, labels = data.cuda(), labels.cuda()
+    data, labels = Variable(data), Variable(labels)
     data = model(data)
-    loss = loss_function(data, labels)
+    loss = criterion(data, labels)
     optimizer.zero_grad()
     loss.backward()
     if args.pretrained_cnn:
@@ -66,9 +81,28 @@ for epoch in range(args.n_epochs):
   ns, nm = 0.0, 0.0
   for index, batch in enumerate(validation_loader):
     data, labels = batch
-    data, labels = Variable(data.cuda()), Variable(labels.cuda())
+    if args.gpu > -1:
+      data, labels = data.cuda(), labels.cuda()
+    data, labels = Variable(data), Variable(labels)
     data = model(data)
     ns += data.size()[0]
     nm += n_matches(data, labels)
   ratio = nm / ns
   print 'epoch %d ratio of matching %f' % (epoch + 1, ratio)
+
+ns, nm = 0.0, 0.0
+for index, batch in enumerate(test_loader):
+  data, labels = batch
+  if args.gpu > -1:
+    data, labels = data.cuda(), labels.cuda()
+  data, labels = Variable(data), Variable(labels)
+  data = model(data)
+  ns += data.size()[0]
+  nm += n_matches(data, labels)
+ratio = nm / ns
+print 'test ratio of matching %f' % ratio
+
+if args.model_path:
+  th.save(model.state_dict(), args.model_path)
+if args.cnn_path:
+  th.save(model._cnn.state_dict(), args.cnn_path)
--- loss.py
+++ loss.py
@@ -1,179 +1,215 @@
+from pdb import set_trace as st
+
 import torch as th
 from torch.autograd import Variable
 import torch.nn.functional as F
 from utilities import onehot, jsd
 
-def loss(data, labels):
-  """ A wrapper for the loss function implemented by Jialin and Helen.
-  data: (N, T, C)
-  labels: (N, T, C) no duplication
-  """
+class Criterion(object):
+  def __init__(self):
+    super(Criterion, self).__init__()
+    self._cuda = False
+    self._contextualize = lambda t: t.cuda() if self._cuda else t
 
-  N, T, _ = data.size()
-  out = {}
-  out['cs'] = th.chunk(data, T, 1)
-  out['cs'] = map(th.squeeze, out['cs'])
-  out['cs'] = map(F.log_softmax, out['cs'])
-  out['cs'] = tuple(th.unsqueeze(ch, 2) for ch in out['cs'])
-  out['cs'] = th.cat(out['cs'], 2)
-  out['ss'] = th.zeros(N, 1, T)
-  out['ss'][:, :, -1] = 1
-  out['ss'] = Variable(out['ss'].cuda())
-  out['ss'] = None
-  _, y = th.max(labels, 2)
-  y = th.squeeze(y)
-  return compute_loss(out, y, True)
+  def __call__(self):
+    raise NotImplementedError()
 
-def compute_loss(out, y, use_cuda, discourage=False, backward_kl=False):
-  """ By Jialin and Helen. """
-  loss = 0.0
-  loss_c = 0.0
-  loss_s = 0.0
-  cs, ss = out['cs'], out['ss']
-  B, C, T = cs.size()
-  preds = th.max(cs, 1)[1].view(-1, T).detach()
-  mask = th.cat([th.zeros(B, C+1).scatter_(1, y.data[:, t].cpu().unsqueeze(1), 1).unsqueeze(2) for t in range(T)],
-                2).sum(2).squeeze()[:, :C]
-  for t in range(T):
-    ymask = (y.data.cpu()[:, t] != 10).float()
-    if use_cuda:
-      mask = mask.cuda()
-      ymask = ymask.cuda()
-    mask = Variable(mask)
-    ymask = Variable(ymask)
-    ones = th.ones(B, 1)
-    if use_cuda:
-      ones = ones.cuda()
-    ones = Variable(ones)
-    if backward_kl:
-      log_labels = th.log((mask + 1e-7) / th.max(th.cat((mask.sum(1), ones), 1), 1)[0].expand_as(mask))
-      loss_c += ymask * (th.exp(cs[:, :, t]) * (cs[:, :, t] - log_labels)).sum(1)
-    elif discourage:
-      loss_c = loss_c - ymask * (cs[:, :, t] * mask).sum(1)
-    else:
-      loss_c = loss_c - ymask * (cs[:, :, t] * mask).sum(1) / th.max(th.cat((mask.sum(1), ones), 1), 1)[0]
-#   loss_s = loss_s - (ymask * th.log(1 - ss[:,:,t] + 1e-7) + (1 - ymask) * th.log(ss[:,:,t] + 1e-7))
-    new_mask = th.zeros(B, C+1).scatter_(1, preds.data.cpu()[:, t].unsqueeze(1), -1)[:, :C] + mask.data.cpu()
-    new_mask[new_mask < 0] = 0
-    if discourage:
-      correct_cls = (new_mask - mask.data.cpu()) < 0
-      mask = new_mask - correct_cls.float() * (new_mask == 0).float()
-    else:
-      mask = new_mask
-  loss += (loss_c + loss_s).mean()
-  return loss
+  def cuda(self):
+    self._cuda = True
 
-def loss(data, labels):
-  """ A re-implementation of the loss function implemented by Jialin and Helen.
+class semi_cross_entropy(Criterion):
+  def __init__(self):
+    super(semi_cross_entropy, self).__init__()
 
-  data (N, T, C)
-  labels (N, T, C) no duplication
-  """
+  def __call__(self, data, labels):
+    """ A wrapper for the loss function implemented by Jialin and Helen.
+    data: (N, T, C)
+    labels: (N, T, C) no duplication
+    """
 
-  N, T, C = data.size()
+    N, T, _ = data.size()
+    out = {}
+    out['cs'] = th.chunk(data, T, 1)
+    out['cs'] = map(th.squeeze, out['cs'])
+    out['cs'] = map(F.log_softmax, out['cs'])
+    out['cs'] = tuple(th.unsqueeze(ch, 2) for ch in out['cs'])
+    out['cs'] = th.cat(out['cs'], 2)
+    out['ss'] = th.zeros(N, 1, T)
+    out['ss'][:, :, -1] = 1
+    out['ss'] = Variable(self._contextualize(out['ss']))
+    out['ss'] = None
+    _, y = th.max(labels, 2)
+    y = th.squeeze(y)
+    return self._compute_loss(out, y, self._cuda)
 
-  # partition an array with shape (N, T, C) into T arrays with shape (N, C)
-  chunks = th.chunk(data, T, 1)
-  chunks = map(th.squeeze, chunks)
+  @staticmethod
+  def _compute_loss(out, y, use_cuda, discourage=False, backward_kl=False):
+    """ By Jialin and Helen. """
+    loss = 0.0
+    loss_c = 0.0
+    loss_s = 0.0
+    cs, ss = out['cs'], out['ss']
+    B, C, T = cs.size()
+    preds = th.max(cs, 1)[1].view(-1, T).detach()
+    mask = th.cat([th.zeros(B, C+1).scatter_(1, y.data[:, t].cpu().unsqueeze(1), 1).unsqueeze(2) for t in range(T)],
+                  2).sum(2).squeeze()[:, :C]
+    for t in range(T):
+      ymask = (y.data.cpu()[:, t] != 10).float()
+      if use_cuda:
+        mask = mask.cuda()
+        ymask = ymask.cuda()
+      mask = Variable(mask)
+      ymask = Variable(ymask)
+      ones = th.ones(B, 1)
+      if use_cuda:
+        ones = ones.cuda()
+      ones = Variable(ones)
+      if backward_kl:
+        log_labels = th.log((mask + 1e-7) / th.max(th.cat((mask.sum(1), ones), 1), 1)[0].expand_as(mask))
+        loss_c += ymask * (th.exp(cs[:, :, t]) * (cs[:, :, t] - log_labels)).sum(1)
+      elif discourage:
+        loss_c = loss_c - ymask * (cs[:, :, t] * mask).sum(1)
+      else:
+        loss_c = loss_c - ymask * (cs[:, :, t] * mask).sum(1) / th.max(th.cat((mask.sum(1), ones), 1), 1)[0]
+      # loss_s = loss_s - (ymask * th.log(1 - ss[:,:,t] + 1e-7) + (1 - ymask) * th.log(ss[:,:,t] + 1e-7))
+      new_mask = th.zeros(B, C+1).scatter_(1, preds.data.cpu()[:, t].unsqueeze(1), -1)[:, :C] + mask.data.cpu()
+      new_mask[new_mask < 0] = 0
+      if discourage:
+        correct_cls = (new_mask - mask.data.cpu()) < 0
+        mask = new_mask - correct_cls.float() * (new_mask == 0).float()
+      else:
+        mask = new_mask
+    loss += (loss_c + loss_s).mean()
+    return loss
 
-  chunks = map(F.log_softmax, chunks)
+class alternative_semi_cross_entropy(Criterion):
+  def __init__(self):
+    super(alternative_semi_cross_entropy, self).__init__()
 
-  # one-hot encoding of c_t
-  mask = th.sum(labels.data, 1)
-  mask = th.squeeze(mask)
+  def __call__(self, data, labels):
+    """ A re-implementation of the loss function implemented by Jialin and Helen.
 
-  loss = 0
-  for index, chunk in enumerate(chunks):
-    # 1 / |c_t| \sum_{c \in c_t} \log(p_c)
-    loss += th.sum(Variable(mask) * chunk, 1) / (T - index)
+    data (N, T, C)
+    labels (N, T, C) no duplication
+    """
 
-    # remove prediction from c_t
-    _, p = th.max(chunk, 1)
-    onehot_p = onehot(p.data, 10)
-    mask = mask - onehot_p
+    N, T, C = data.size()
 
-    # in case the prediction does not belong to c_t
-    mask = th.max(th.zeros(mask.size()).cuda(), mask)
+    # partition an array with shape (N, T, C) into T arrays with shape (N, C)
+    chunks = th.chunk(data, T, 1)
+    chunks = map(th.squeeze, chunks)
 
-  # likelihodd maximization is equivalent to negtive likelihood minimization
-  loss = -th.mean(loss)
+    chunks = map(F.log_softmax, chunks)
 
-  return loss
+    # one-hot encoding of c_t
+    mask = th.sum(labels.data, 1)
+    mask = th.squeeze(mask)
 
-def loss(data, labels):
-  """
-  Instead of computing loss step by step, this loss function aggregates distributions
-  along temporal axis and only considers aggregated distributions.
-  This loss function consists of 
-    - Jensen-Shannon divergence between (aggregated) predicted and targeted distribution
-    - An entropy-based regularizer ensuring one-peak behavior of predicted distribution
+    loss = 0
+    for index, chunk in enumerate(chunks):
+      # 1 / |c_t| \sum_{c \in c_t} \log(p_c)
+      loss += th.sum(Variable(mask) * chunk, 1) / (T - index)
 
-  data (N, T, C)
-  labels (N, T, C) no duplication
-  """
+      # remove prediction from c_t
+      _, p = th.max(chunk, 1)
+      onehot_p = onehot(p.data, 10, self._cuda)
+      mask = mask - onehot_p
 
-  _, T, _ = data.size()
-  data = th.chunk(data, T, 1)
-  data = map(th.squeeze, data)
-  data = map(F.softmax, data)
-  data = map(lambda t: th.unsqueeze(t, 1), data)
-  data = th.cat(data, 1)
+      # in case the prediction does not belong to c_t
+      mask = th.max(self._contextualize(th.zeros(mask.size())), mask)
 
-  # aggregates distributions along temporal axis
-  data = th.mean(data, 1)
-  data = th.squeeze(data)
-  labels = th.mean(labels, 1)
-  labels = th.squeeze(labels)
+    # likelihodd maximization is equivalent to negtive likelihood minimization
+    loss = -th.mean(loss)
 
-  # jsd
-  div = jsd(data, labels)
+    return loss
 
-  # for numerical stability
-  threshold = Variable(th.ones(data.size()) * 1e-5).cuda()
-  data = th.max(threshold, data)
+class kl_loss(Criterion):
+  def __init__(self):
+    super(kl_loss, self).__init__()
 
-  # entropy regularizer
-  entropy = th.mean(data * th.log(data))
+  def __call__(self, data, labels):
+    """
+    Instead of computing loss step by step, this loss function aggregates distributions
+    along temporal axis and only considers aggregated distributions.
+    This loss function consists of 
+      - Jensen-Shannon divergence between (aggregated) predicted and targeted distribution
+      - An entropy-based regularizer ensuring one-peak behavior of predicted distribution
 
-  return div + entropy
+    data (N, T, C)
+    labels (N, T, C) no duplication
+    """
 
-def loss(data, labels):
-  """ Loss function based on reinforcement learning.
+    _, T, _ = data.size()
+    data = th.chunk(data, T, 1)
+    data = map(th.squeeze, data)
+    data = map(F.softmax, data)
+    data = map(lambda t: th.unsqueeze(t, 1), data)
+    data = th.cat(data, 1)
 
-  data (N, T, C)
-  labels (N, T, C) no duplication
-  """
-  
-  N, T, C = data.size()
+    # aggregates distributions along temporal axis
+    data = th.mean(data, 1)
+    data = th.squeeze(data)
+    labels = th.mean(labels, 1)
+    labels = th.squeeze(labels)
 
-  # partition an array with shape (N, T, C) into T arrays with shape (N, C)
-  chunks = th.chunk(data, T, 1)
-  chunks = map(th.squeeze, chunks)
+    # jsd
+    div = jsd(data, labels)
 
-  chunks = map(F.log_softmax, chunks)
+    # for numerical stability
+    threshold = th.ones(data.size()) * 1e-5
+    threshold = self._contextualize(threshold)
+    threshold = Variable(threshold)
+    data = th.max(threshold, data)
 
-  # c stands for c_t
-  c = th.sum(labels.data, 1)
-  c = th.squeeze(c)
+    # entropy regularizer
+    entropy = th.mean(data * th.log(data))
 
-  loss = 0
-  for index, chunk in enumerate(chunks):
-    # compute reward (reward set to 1 if prediction belongs to c_t, -1 otherwise)
-    _, p = th.max(chunk, 1)
-    onehot_p = onehot(p.data, 10)
-    belonging_to = th.sum(c * onehot_p, 1) # whether prediction belongs to c_t
-    belonging_to = 1 - belonging_to # reverse
-    belonging_to = belonging_to.expand_as(onehot_p) # broadcast
-    offset = -2 * onehot_p * belonging_to
-    reward = onehot_p + offset # set reward as -1 for misprediction
-    reward = Variable(reward).cuda()
+    return div + entropy
 
-    loss += th.sum(chunk * reward)
+class rl_loss(Criterion):
+  def __init__(self):
+    super(rl_loss, self).__init__()
 
-    c = c - onehot_p # remove prediction from c_t
-    c = th.max(th.zeros(c.size()).cuda(), c) # in case of misprediction
+  def __call__(self, data, labels):
+    """ Loss function based on reinforcement learning.
 
-  # reward maximization is equivalent to negtive reward minimization
-  loss = -loss
+    data (N, T, C)
+    labels (N, T, C) no duplication
+    """
+    
+    N, T, C = data.size()
 
-  return loss
+    # partition an array with shape (N, T, C) into T arrays with shape (N, C)
+    chunks = th.chunk(data, T, 1)
+    chunks = map(th.squeeze, chunks)
+
+    chunks = map(F.log_softmax, chunks)
+
+    # c stands for c_t
+    c = th.sum(labels.data, 1)
+    c = th.squeeze(c)
+
+    loss = 0
+    for index, chunk in enumerate(chunks):
+      # compute reward (reward set to 1 if prediction belongs to c_t, -1 otherwise)
+      _, p = th.max(chunk, 1)
+      onehot_p = onehot(p.data, 10, self._cuda)
+      belonging_to = th.sum(c * onehot_p, 1) # whether prediction belongs to c_t
+      belonging_to = 1 - belonging_to # reverse
+      belonging_to = belonging_to.expand_as(onehot_p) # broadcast
+      offset = -2 * onehot_p * belonging_to
+      reward = onehot_p + offset # set reward as -1 for misprediction
+      reward = self._contextualize(reward)
+      reward = Variable(reward)
+
+      loss += th.sum(chunk * reward)
+
+      c = c - onehot_p # remove prediction from c_t
+      threshold = th.zeros(c.size())
+      threshold = self._contextualize(threshold)
+      c = th.max(threshold, c) # in case of misprediction
+
+    # reward maximization is equivalent to negtive reward minimization
+    loss = -loss
+
+    return loss
