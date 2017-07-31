2e576e05ed062d672bada3136d2065331e2c8ac0
zzhang-cn <zzhang@gmail.com>
2017-07-31 11:35:16 +0800

    evalate.py: indentation and gpu use

--- evaluate.py
+++ evaluate.py
@@ -12,6 +12,7 @@ parser = ArgumentParser()
 parser.add_argument('--cnn-path', type=str, default='pretrained-cnn')
 parser.add_argument('--batch-size', type=int, default=64)
 parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--use_gpu', type=bool, default=False)
 parser.add_argument('--interval', type=int, default=100)
 parser.add_argument('--n', type=int, default=3)
 parser.add_argument('--n-epochs', type=int, default=10)
@@ -20,7 +21,8 @@ parser.add_argument('--pretrained-cnn', action='store_true', default=False)
 args = parser.parse_args()
 print args
 
-th.cuda.set_device(args.gpu)
+if args.use_gpu:
+    th.cuda.set_device(args.gpu)
 
 training_data, training_labels = joblib.load('training-%d.data' % args.n)
 training_data = th.from_numpy(training_data)
@@ -42,43 +44,47 @@ test_loader = DataLoader(test_set, args.batch_size)
 
 cnn_path = args.cnn_path if args.pretrained_cnn else None
 model = RNN(args.n_units, 10, cnn_path)
-model.cuda()
+if args.use_gpu:
+    model.cuda()
 optimizer = Adam(model.parameters(), lr=1e-3)
 
 for epoch in range(args.n_epochs):
-  for index, batch in enumerate(training_loader):
-    data, labels = batch
-    data, labels = Variable(data.cuda()), Variable(labels.cuda())
-    data = model(data)
-    loss = loss_function(data, labels)
-    optimizer.zero_grad()
-    loss.backward()
-    if args.pretrained_cnn:
-      model.cnn_zero_grad()
-    optimizer.step()
+    for index, batch in enumerate(training_loader):
+        data, labels = batch
+        if args.use_gpu:
+            data, labels = Variable(data.cuda()), Variable(labels.cuda())
+        data = model(data)
+        loss = loss_function(data, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        if args.pretrained_cnn:
+            model.cnn_zero_grad()
+        optimizer.step()
 
-    if (index + 1) % args.interval == 0:
-      ns = float(data.size()[0])
-      nm = float(n_matches(data, labels))
-      ratio = nm / ns
-      print 'batch %d training loss %f ratio of matching %f' % (index + 1, loss.data[0], ratio)
+        if (index + 1) % args.interval == 0:
+            ns = float(data.size()[0])
+            nm = float(n_matches(data, labels))
+            ratio = nm / ns
+            print 'batch %d training loss %f ratio of matching %f' % (index + 1, loss.data[0], ratio)
 
-  ns, nm = 0.0, 0.0
-  for index, batch in enumerate(training_loader):
-    data, labels = batch
-    data, labels = Variable(data.cuda()), Variable(labels.cuda())
-    data = model(data)
-    ns += data.size()[0]
-    nm += n_matches(data, labels)
-  ratio = nm / ns
-  print 'epoch %d ratio of matching %f' % (epoch + 1, ratio)
+    ns, nm = 0.0, 0.0
+    for index, batch in enumerate(validation_loader):
+        data, labels = batch
+        if args.use_gpu:
+            data, labels = Variable(data.cuda()), Variable(labels.cuda())
+        data = model(data)
+        ns += data.size()[0]
+        nm += n_matches(data, labels)
+        ratio = nm / ns
+        print 'epoch %d ratio of matching %f' % (epoch + 1, ratio)
 
 ns, nm = 0.0, 0.0
 for index, batch in enumerate(test_loader):
-  data, labels = batch
-  data, labels = Variable(data.cuda()), Variable(labels.cuda())
-  data = model(data)
-  ns += data.size()[0]
-  nm += n_matches(data, labels)
-print 'epoch %d total number of errors %d' % (epoch + 1, ratio)
+    data, labels = batch
+    if args.use_gpu:
+        data, labels = Variable(data.cuda()), Variable(labels.cuda())
+    data = model(data)
+    ns += data.size()[0]
+    nm += n_matches(data, labels)
+    print 'epoch %d total number of errors %d' % (epoch + 1, ratio)
 # import pdb; pdb.set_trace()
